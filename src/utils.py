7d8c79b06624c6c23bcea29b30a68f6f5333f24c
fornari03 <221017023@aluno.unb.br>
2025-04-27 18:57:26 -0300

    feat: implementa binary_string_to_nibble_matrix()

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,9 @@ def string_to_binary_string(msg: str) -> str:
     elif len(bin_str) > 16:
         return bin_str[:16]  # corta se passar
     return bin_str
+
+
+def binary_string_to_nibble_matrix(bin_str: str):
+    # converte a string binária em uma matriz de nibbles
+    nibbles = [int(bin_str[i:i+4], 2) for i in range(0, 16, 4)]
+    return [[nibbles[0], nibbles[1]], [nibbles[2], nibbles[3]]]
