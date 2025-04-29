794ac111e53f1bfcca272d8ed4ef407ba634ca86
fornari03 <221017023@aluno.unb.br>
2025-04-29 11:29:53 -0300

    feat: ajusta addRoundKey e termina algoritmo do S-AES

--- src/add_round_key.py
+++ src/add_round_key.py
@@ -2,6 +2,8 @@ from utils import print_ret
 
 @print_ret
 def addRoundKey(state, nibble_matrix_key):
+    # primeiro ajusta a matriz de nibbles da chave
+    nibble_matrix_key[0][1], nibble_matrix_key[1][0] = nibble_matrix_key[1][0], nibble_matrix_key[0][1]
     for i in range(2):
         for j in range(2):
             state[i][j] = state[i][j] ^ nibble_matrix_key[i][j]
--- src/main.py
+++ src/main.py
@@ -2,24 +2,43 @@ from utils import string_to_binary_string, binary_string_to_nibble_matrix
 from key_expansion import keyExpansion
 from add_round_key import addRoundKey
 from substitute_nibbles import substituteNibbles
+from shift_rows import shiftRows
+from mix_columns import mixColumns
+
+import base64
 
 message = input()
 message = string_to_binary_string(message)
-message = binary_string_to_nibble_matrix(message)
+in_matrix = binary_string_to_nibble_matrix(message)
+print(f"Início: {in_matrix}")
 
-key = [0xA, 0xB, 0xC, 0xD] # chave de 16 bits em 4 nibbles
+# key = [0x7, 0x1, 0x4, 0x9] # chave de 16 bits em 4 nibbles
+key = [0xA, 0x7, 0x3, 0xB] # chave de 16 bits em 4 nibbles
 
-nibble_keys = keyExpansion(key)
+# in_matrix = [[3,1],[15, 11]]
+round_keys = keyExpansion(key)
+print(f"Chave expandidas:")
+for i in range(len(round_keys)):
+    print(f"w{i}: {round_keys[i]}")
+print()
 
-state = addRoundKey(message, nibble_keys[0:2]) # w0 e w1
+state = addRoundKey(in_matrix, round_keys[0:2]) # w0 e w1
 
 # primeira rodada:
 state = substituteNibbles(state)
-# state = shiftRows(state)
-# state = mixColumns(state)
-state = addRoundKey(state, nibble_keys[2:4]) # w2 e w3
+
+state = shiftRows(state)
+
+state = mixColumns(state)
+
+state = addRoundKey(state, round_keys[2:4]) # w2 e w3
 
 # segunda rodada:
+state = substituteNibbles(state)
 
-# mostra o resultado em base64
-# print(base64.b64encode(result))
+state = shiftRows(state)
+
+out_matrix = addRoundKey(state, round_keys[4:6]) # w4 e w5
+
+
+# TODO: transforma a matriz de nibbles em uma string binária
