f8c961c5ff653c7bc367acdec54c2a3a468afeee
fornari03 <221017023@aluno.unb.br>
2025-04-29 11:28:39 -0300

    feat: implementa mixColumns

---
+++ src/mix_columns.py
@@ -0,0 +1,34 @@
+from utils import print_ret
+
+@print_ret
+def mixColumns(state):
+    # faz a multiplicação de matriz em GF(2⁴)
+    mix_matrix = [[1, 4], [4, 1]] # matriz fixa do S-AES
+    result = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            result[i][j] = galois_mult(mix_matrix[i][0], state[0][j]) ^ galois_mult(mix_matrix[i][1], state[1][j])
+    return result
+
+def galois_mult(a, b):
+    # multiplicação no campo de Galois GF(2⁴)
+    result = 0
+
+    for i in range(4):
+        # Se o bit menos significativo de b for 1, fazemos XOR do acumulador com 'a'
+        if b & 1: 
+            result ^= a
+
+        # se o MSB de 'a' (em 4 bits) for 1, vai ter carry
+        carry = a & 0b1000
+
+        # multiplica 'a' por 2 e mantém apenas 4 bits
+        a = (a << 1) & 0b1111
+
+        if carry:
+            a ^= 0b10011 # x⁴ + x + 1
+
+        b >>= 1 # volta o loop e analisa o próximo bit de b
+
+    # pega os 4 LSB
+    return result & 0b1111
