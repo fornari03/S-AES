d6b9204cd5a003e769695d48f54eefae8dfc7993
fornari03 <221017023@aluno.unb.br>
2025-04-29 12:11:05 -0300

    feat: faz saídas em hexa e base64

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from utils import string_to_binary_string, binary_string_to_nibble_matrix
+from utils import string_to_binary_string, binary_string_to_nibble_matrix, nibble_matrix_to_binary_string, binary_string_to_base64, binary_string_to_hex
 from key_expansion import keyExpansion
 from add_round_key import addRoundKey
 from substitute_nibbles import substituteNibbles
 from shift_rows import shiftRows
 from mix_columns import mixColumns
 
-import base64
 
 message = input()
 message = string_to_binary_string(message)
@@ -41,4 +40,16 @@ state = shiftRows(state)
 out_matrix = addRoundKey(state, round_keys[4:6]) # w4 e w5
 
 
-# TODO: transforma a matriz de nibbles em uma string binária
+print("Matriz de saída:")
+for row in out_matrix:
+    print(row)
+
+cipher_text = nibble_matrix_to_binary_string(out_matrix)
+cipher_text_spaced = ' '.join(cipher_text[i:i+4] for i in range(0, len(cipher_text), 4))
+print(f"Texto cifrado em binário: {cipher_text_spaced}")
+
+cipher_text_hex = binary_string_to_hex(cipher_text)
+print(f"Texto cifrado em hexadecimal: 0x{cipher_text_hex}")
+
+cipher_text = binary_string_to_base64(cipher_text)
+print(f"Texto cifrado em base64: {cipher_text}")
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-def string_to_binary_string(msg: str) -> str:
+import base64
+
+
+def string_to_binary_string(msg):
     bin_str = ''.join(format(ord(i), '08b') for i in msg)
     if len(bin_str) < 16:
         return bin_str.ljust(16, '0')  # padding com 0s
@@ -7,10 +10,28 @@ def string_to_binary_string(msg: str) -> str:
     return bin_str
 
 
-def binary_string_to_nibble_matrix(bin_str: str):
+def binary_string_to_nibble_matrix(bin_str):
     # converte a string binária em uma matriz de nibbles
     nibbles = [int(bin_str[i:i+4], 2) for i in range(0, 16, 4)]
     return [[nibbles[0], nibbles[2]], [nibbles[1], nibbles[3]]]
+
+
+def nibble_matrix_to_binary_string(matrix):
+    # converte a matriz de nibbles de volta para uma string binária
+    nibbles = [matrix[0][0], matrix[1][0], matrix[0][1], matrix[1][1]]
+    return ''.join(format(nibble, '04b') for nibble in nibbles)
+
+def binary_string_to_base64(bin_str):
+    # converte a string binária em uma string base64
+    n = int(bin_str, 2)
+    b64_str = base64.b64encode(n.to_bytes((n.bit_length() + 7) // 8, 'big')).decode()
+    return b64_str
+
+def binary_string_to_hex(bin_str):
+    # converte a string binária em uma string hexadecimal
+    n = int(bin_str, 2)
+    hex_str = hex(n)[2:].upper()
+    return hex_str.zfill(4)  # preenche com zeros à esquerda para garantir 4 dígitos
 
 
 def print_ret(func):
@@ -23,5 +44,10 @@ def print_ret(func):
 
 def print_matrix(matrix):
     for row in matrix:
-        print(row)
+        row = [int_to_hex(num) for num in row]
+        print(f"[{', '.join(row)}]")
     print()
+
+def int_to_hex(n):
+    # converte um inteiro em uma string hexadecimal
+    return hex(n)[2:].upper()
